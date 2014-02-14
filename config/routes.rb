cb8ee97651945af3638aa31670ae13ac5b9d7236
sspablo23 <sspablo23@hotmail.com>
2014-02-14 17:32:17 -0600

    Added routes to the route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 MockStubDemo::Application.routes.draw do
+  root to: 'banks#index'
+  resources :banks
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
